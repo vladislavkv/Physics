d8461831d3ae21af225df7e22f7774015f6186ab
Imagination <vladislavkv@bk.ru>
2018-03-31 17:39:15 +0600

    Initial commit

---
+++ Fizika/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Fizika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.задачиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.калькуляторToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограмеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оРазработчикеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.версииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.задачиToolStripMenuItem,
+            this.калькуляторToolStripMenuItem,
+            this.оПрограмеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(333, 26);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // задачиToolStripMenuItem
+            // 
+            this.задачиToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.задачиToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.задачиToolStripMenuItem.Name = "задачиToolStripMenuItem";
+            this.задачиToolStripMenuItem.Size = new System.Drawing.Size(64, 22);
+            this.задачиToolStripMenuItem.Text = "Задачи";
+            // 
+            // калькуляторToolStripMenuItem
+            // 
+            this.калькуляторToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.калькуляторToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.калькуляторToolStripMenuItem.Name = "калькуляторToolStripMenuItem";
+            this.калькуляторToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.калькуляторToolStripMenuItem.Text = "Калькулятор";
+            this.калькуляторToolStripMenuItem.Click += new System.EventHandler(this.калькуляторToolStripMenuItem_Click);
+            // 
+            // оПрограмеToolStripMenuItem
+            // 
+            this.оПрограмеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оРазработчикеToolStripMenuItem,
+            this.версииToolStripMenuItem});
+            this.оПрограмеToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.оПрограмеToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.оПрограмеToolStripMenuItem.Name = "оПрограмеToolStripMenuItem";
+            this.оПрограмеToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.оПрограмеToolStripMenuItem.Text = "О програме";
+            // 
+            // оРазработчикеToolStripMenuItem
+            // 
+            this.оРазработчикеToolStripMenuItem.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.оРазработчикеToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.оРазработчикеToolStripMenuItem.Name = "оРазработчикеToolStripMenuItem";
+            this.оРазработчикеToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.оРазработчикеToolStripMenuItem.Text = "О разработчике";
+            // 
+            // версииToolStripMenuItem
+            // 
+            this.версииToolStripMenuItem.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.версииToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.версииToolStripMenuItem.Name = "версииToolStripMenuItem";
+            this.версииToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.версииToolStripMenuItem.Text = "Версии";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.выходToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(61, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(333, 400);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Физика";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem задачиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem калькуляторToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограмеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оРазработчикеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem версииToolStripMenuItem;
+    }
+}
+
---
+++ Fizika/Form2.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fizika
+{
+    public partial class Form2 : Form
+    {
+
+        double c1, c3, res;
+        string c2;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "3,14";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text += "9";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            c1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "+";
+            c2 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            c1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "-";
+            c2 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            c1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "*";
+            c2 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            c1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "/";
+            c2 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            c3 = Convert.ToDouble(textBox1.Text);
+            if (c2 == "+")
+            {
+                res = c1 + c3;
+                textBox1.Text = Convert.ToString(res);
+            }
+            if (c2 == "*")
+            {
+                res = c1 * c3;
+                textBox1.Text = Convert.ToString(res);
+            }
+            if (c2 == "/")
+            {
+                res = c1 / c3;
+                textBox1.Text = Convert.ToString(res);
+            }
+            if (c2 == "-")
+            {
+                res = c1 - c3;
+                textBox1.Text = Convert.ToString(res);
+            }
+            if (c2 == "^")
+            {
+                for (int i = 0; i < c3; i++)
+                {
+                    res = c1 * c1;
+                }
+                textBox1.Text = Convert.ToString(res);
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            c1 = 0;
+            c2 = "";
+            c3 = 0;
+            res = 0;
+            textBox1.Text = "";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            c1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "^";
+            c2 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            res = Math.Sqrt(a);
+            textBox1.Text = Convert.ToString(res);
+        }
+
+        private void назадToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 myForm1 = new Form1();
+            myForm1.Show();
+            this.Hide();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int n = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ',')
+                {
+                    n++;
+                }
+            }
+
+            if (n < 1)
+            {
+                textBox1.Text = textBox1.Text + ",";
+            }
+        }
+    }
+}
