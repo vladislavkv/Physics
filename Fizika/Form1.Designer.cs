fd770244cbb57e2306285d9df3e1b87790cb8fe8
Imagination <vladislavkv@bk.ru>
2018-04-07 16:10:44 +0600

    2

--- Fizika/Form1.Designer.cs
+++ Fizika/Form1.Designer.cs
@@ -40,13 +40,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -119,7 +118,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(224, 39);
+            this.button1.Location = new System.Drawing.Point(224, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 43);
             this.button1.TabIndex = 1;
@@ -130,7 +129,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(224, 88);
+            this.button2.Location = new System.Drawing.Point(224, 119);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(97, 43);
             this.button2.TabIndex = 1;
@@ -140,7 +139,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(224, 137);
+            this.button3.Location = new System.Drawing.Point(224, 168);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(97, 43);
             this.button3.TabIndex = 1;
@@ -150,7 +149,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(224, 186);
+            this.button4.Location = new System.Drawing.Point(224, 217);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(97, 43);
             this.button4.TabIndex = 1;
@@ -160,96 +159,85 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.Location = new System.Drawing.Point(224, 235);
+            this.button5.Location = new System.Drawing.Point(224, 266);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(97, 43);
             this.button5.TabIndex = 1;
             this.button5.Text = "Открыть";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button6
-            // 
-            this.button6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button6.Location = new System.Drawing.Point(224, 284);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(97, 43);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "Открыть";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button7.Location = new System.Drawing.Point(12, 39);
+            this.button7.Location = new System.Drawing.Point(12, 70);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(206, 43);
             this.button7.TabIndex = 1;
-            this.button7.Text = "Открыть";
+            this.button7.Text = "Протоны и нейтроны\r\n";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button8.Location = new System.Drawing.Point(12, 88);
+            this.button8.Location = new System.Drawing.Point(12, 119);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(206, 43);
             this.button8.TabIndex = 1;
-            this.button8.Text = "Открыть";
+            this.button8.Text = "Тема 2";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button9.Location = new System.Drawing.Point(12, 137);
+            this.button9.Location = new System.Drawing.Point(12, 168);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(206, 43);
             this.button9.TabIndex = 1;
-            this.button9.Text = "Открыть";
+            this.button9.Text = "Тема 3";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button10
             // 
             this.button10.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button10.Location = new System.Drawing.Point(12, 235);
+            this.button10.Location = new System.Drawing.Point(12, 266);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(206, 43);
             this.button10.TabIndex = 1;
-            this.button10.Text = "Открыть";
+            this.button10.Text = "Тема 5";
             this.button10.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            this.button11.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button11.Location = new System.Drawing.Point(12, 284);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(206, 43);
-            this.button11.TabIndex = 1;
-            this.button11.Text = "Открыть";
-            this.button11.UseVisualStyleBackColor = true;
             // 
             // button12
             // 
             this.button12.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button12.Location = new System.Drawing.Point(12, 186);
+            this.button12.Location = new System.Drawing.Point(12, 217);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(206, 43);
             this.button12.TabIndex = 1;
-            this.button12.Text = "Открыть";
+            this.button12.Text = "Тема 4";
             this.button12.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбор темы:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(333, 339);
-            this.Controls.Add(this.button6);
+            this.ClientSize = new System.Drawing.Size(333, 321);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -282,13 +270,12 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Fizika/Form3.Designer.cs
+++ Fizika/Form3.Designer.cs
@@ -31,6 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -47,21 +55,99 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Location = new System.Drawing.Point(13, 81);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(635, 144);
             this.label2.TabIndex = 0;
             this.label2.Text = resources.GetString("label2.Text");
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(634, 90);
+            this.label3.TabIndex = 0;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(13, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(636, 90);
+            this.label4.TabIndex = 0;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(13, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(625, 90);
+            this.label5.TabIndex = 0;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(13, 559);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(616, 72);
+            this.label6.TabIndex = 0;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(13, 649);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(370, 18);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Данная ядерная реакция выглядит следующим образом:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fizika.Properties.Resources.f114;
+            this.pictureBox1.Location = new System.Drawing.Point(389, 628);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 39);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(30, 667);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(8, 18);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "\r\n";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 1061);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(655, 626);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form3";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +157,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
     }
 }
